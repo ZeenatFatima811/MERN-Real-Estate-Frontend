37fb994adb90adf09c2921d0e334d458cbb5bdc5
Currency-Converter <z8784507@gmail.com>
2026-04-08 01:02:30 +0500

    app crash error removal 2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,27 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import useReducer  from './user/userSlice';
-import {persistReducer, persistStore} from "redux-persist";
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
-const rootReducer= combineReducers({user: useReducer});
+const rootReducer = combineReducers({
+  user: userReducer,
+});
 
 const persistConfig = {
   key: 'root',
   storage,
   version: 1,
   whitelist: ['user'],
-  blacklist: ['loading', 'error'], 
-}
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: (getDefaultMiddleware)=> getDefaultMiddleware({
-    serializableCheck: false,
-  }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
-export const persistor =persistStore(store);
-
+export const persistor = persistStore(store);
